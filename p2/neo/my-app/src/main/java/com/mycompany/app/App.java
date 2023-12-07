e7d49e78c0e0b75a78696a56d2a57189d03322fc
bbuukk <bohdan.buchok@ukma.edu.ua>
2023-12-07 08:56:04 +0200

    no-imp

---
+++ p2/neo/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,52 @@
+package com.mycompany.app;
+
+import org.neo4j.driver.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        String uri = "bolt://localhost:7687";
+        String username = "user";
+        String password = "password";
+
+        try (Driver driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password));
+                Session session = driver.session()) {
+            System.out.println("Connected...");
+
+            session.run("CREATE " +
+                    "(alice:User {name: 'Alice'}), " +
+                    "(bob:User {name: 'Bob'}), " +
+                    "(carol:User {name: 'Carol'}), " +
+                    "(dave:User {name: 'Dave'})," +
+                    "(sam:User {name: 'Sam'})");
+
+            session.run(
+                    "MATCH (alice:User {name: 'Alice'}), (bob:User {name: 'Bob'}) CREATE (alice)-[:FRIENDS_WITH]->(bob)");
+            session.run(
+                    "MATCH (bob:User {name: 'Bob'}), (carol:User {name: 'carol'}) CREATE (bob)-[:FRIENDS_WITH]->(carol)");
+
+            session.run(
+                    "MATCH (bob:User {name: 'Bob'}), (carol:User {name: 'Carol'}) CREATE (bob)-[:FRIENDS_WITH]->(carol)");
+            session.run(
+                    "MATCH (alice:User {name: 'Alice'}), (dave:User {name: 'Dave'}) CREATE (alice)-[:FRIENDS_WITH]->(dave)");
+
+            session.run(
+                    "MATCH (dave:User {name: 'Dave'}), (carol:User {name: 'Carol'}) CREATE (dave)-[:FRIENDS_WITH]->(carol)");
+            session.run(
+                    "MATCH (dave:User {name: 'Dave'}), (sam:User {name: 'Sam'}) CREATE (dave)-[:FRIENDS_WITH]->(sam)");
+
+            // FRIENDS OF FRIENDS
+            // MATCH (user:User {name:
+            // 'Alice'})-[:FRIENDS_WITH]->(friend:User)-[:FRIENDS_WITH]->(fof:User)
+            // WHERE NOT (user)-[:FRIENDS_WITH]->(fof) AND user <> fof
+            // RETURN DISTINCT fof.name
+
+            // DELETE ALL
+            // MATCH (user:User)-[relationship]-()
+            // DETACH DELETE user, relationship;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ p2/neo/my-app/src/main/java/com/mycompany/app/Vars.java
@@ -0,0 +1,8 @@
+package com.mycompany.app;
+
+public class Vars {
+    public static final String AUTH_KEY = "GRVf0Xd2LFVZSdt0hwXcU1dRp";
+    public static final String AUTH_SECRET = "i3pNJbeEUTSluqZ3NrbLmWMvA1eH5j1RxOP8EjQtZbIWgYPJco";
+    public static final String ACCESS_TOKEN = "1688665677752668161-E1L4LAt0knru7cOZeo9j7Z7PV6iNqh";
+    public static final String ACCESS_SECRET = "XsL5DW1y3ueJO62DDVoKNXrDGGZw4Szz9vsUw737GsCJ4";
+}
